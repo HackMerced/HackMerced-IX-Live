bb9ba1632dd6788d13ad652348e4e43aa54f5891
shawnduong <me@shawnd.xyz>
2024-02-23 21:42:49 -0800

    Implemented to password page front-end.

--- frontend/route.py
+++ frontend/route.py
@@ -15,6 +15,11 @@ def register():
 
 	return render_template("register.html")
 
+@app.route("/login2", methods=["GET"])
+def login2():
+
+	return render_template("login2.html")
+
 #@app.route("/app", methods=["GET"])
 #@login_required
 #def application():
