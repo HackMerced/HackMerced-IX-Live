b97b78aff92d1fe800950d61dbc8f4b76e8c31a2
Shyam Gupta <shyam.gupta0723@gmail.com>
2024-02-24 14:10:02 -0800

    Schedule code

--- code/run.py
+++ code/run.py
@@ -2,16 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return render_template('schedule.html')  
 
 @app.route('/template')
 def home():
     return render_template('template.html')
-
-
 
 if __name__ == '__main__':
     app.run()
