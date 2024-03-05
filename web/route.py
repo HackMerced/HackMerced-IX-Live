dd87e110af1bddba5a2098205b86ffb19c4308e3
Shawn Duong <me@shawnd.xyz>
2024-03-05 12:57:10 -0800

    Moved frontend to new directory.

--- web/route.py
+++ web/route.py
@@ -1,4 +1,53 @@
-from routes.index import *
-from routes.app import *
-from routes.event import *
-from routes.admin import *
+from app import *
+
+#from flask_login import current_user, login_required
+
+@app.route("/", methods=["GET"])
+@app.route("/login", methods=["GET"])
+def index():
+
+#	if current_user.is_authenticated:
+#		return redirect(url_for("application"))
+	return render_template("index.html")
+
+@app.route("/register", methods=["GET"])
+def register():
+
+	return render_template("register.html")
+
+@app.route("/password", methods=["GET"])
+def login2():
+
+	return render_template("password.html")
+
+@app.route("/app", methods=["GET"])
+#@login_required
+def application():
+
+	return render_template("app.html")
+
+@app.route("/prizes", methods=["GET"])
+def prizes():
+
+	return render_template("prizes.html")
+
+@app.route("/schedule", methods=["GET"])
+def schedule():
+
+	return render_template("schedule.html")
+
+@app.route("/punches", methods=["GET"])
+def punches():
+
+	return render_template("punches.html")
+
+@app.route("/profile", methods=["GET"])
+def profile():
+
+	return render_template("profile.html")
+
+@app.route("/asset-pack", methods=["GET"])
+def asset_pack():
+
+	return render_template("asset-pack.html")
+
