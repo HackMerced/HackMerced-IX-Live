aed2040c858ea4ebaf8db706e8639d7b484a747d
shawnduong <me@shawnd.xyz>
2024-03-06 01:08:14 -0800

    Added some styling for the schedule.

--- web/static/js-user/events/common.js
+++ web/static/js-user/events/common.js
@@ -1,8 +1,41 @@
 let lastUpdateEvents = null;
 
+/* Update the day counter. */
+const FRIDAY = 1709884800;
+const SATURDAY = 1709971200;
+const SUNDAY = 1710057600;
+const MONDAY = 1710140400;
+
 /* Load events from /api/event/list into #event-table. */
 function load_events()
 {
+	let now = Date.now()/1000;
+
+	if ((now >= FRIDAY) && (now < SATURDAY))
+	{
+		$("#day-friday").addClass("today");
+		$("#day-saturday").removeClass("today");
+		$("#day-sunday").removeClass("today");
+	}
+	else if ((now >= SATURDAY) && (now < SUNDAY))
+	{
+		$("#day-friday").removeClass("today");
+		$("#day-saturday").addClass("today");
+		$("#day-sunday").removeClass("today");
+	}
+	else if ((now >= SUNDAY) && (now < MONDAY))
+	{
+		$("#day-friday").removeClass("today");
+		$("#day-saturday").removeClass("today");
+		$("#day-sunday").addClass("today");
+	}
+	else
+	{
+		$("#day-friday").removeClass("today");
+		$("#day-saturday").removeClass("today");
+		$("#day-sunday").removeClass("today");
+	}
+
 	$.getJSON("/api/event/list", function (d)
 	{
 		let dataStr = JSON.stringify(d.Events);
@@ -18,9 +51,15 @@ function load_events()
 
 			for (let i = d.Events.length-1; i >= 0; i--)
 			{
+				/* Hide past events. */
+				if (d.Events[i].status == 2)  continue;
+
+				let currentClass = "";
+				if (d.Events[i].status == 1)  currentClass = " event-current";
+
 				str =
 					"<tr class='table-spacer'></tr>"+
-					"<tr class='table-content'>"+
+					"<tr class='table-content"+currentClass+"'>"+
 						"<td class='map'><img src='"+d.Events[i].weblink+"'/></td>"+
 						"<td class='info'>"+
 							"<p><b>"+d.Events[i].title+"</b> by "+d.Events[i].author+"</p>"+
